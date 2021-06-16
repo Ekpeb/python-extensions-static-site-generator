6c36206093f00f2a8233672eb8d493eec4e32552
xkang <expebble@outlook.com>
2021-06-16 11:47:38 -0400

    master 4.1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -51,8 +51,7 @@ class MarkdownParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
-
-    hooks.event("written")
+        hooks.event("written")
 
 
 class ReStructuredTextParser(Parser):
@@ -66,5 +65,4 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
-
-    hooks.event("written")
+        hooks.event("written")
